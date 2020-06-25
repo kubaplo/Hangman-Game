8b4820721d8daec5562e8d1284bbfeae6f76d564
SZP3N10 <szp3n10.dev@gmail.com>
2020-06-25 16:08:49 +0200

    'End page' completed.

--- Game.py
+++ Game.py
@@ -17,14 +17,15 @@ class Application(QMainWindow):
         self.setGeometry(int((1920-width)/2), int((1080-height)/2), width, height)
         self.setStyleSheet("background-color: #777")
 
-        start_layout = QtWidgets.QVBoxLayout()
-        start_layout.setAlignment(QtCore.Qt.AlignTop)
-        start_layout_widget = QtWidgets.QWidget(self)
-        start_layout_widget.setLayout(start_layout)
-        start_layout_widget.setGeometry(0, 0, self.width(), self.height())
-        self.start_layout_widget = start_layout_widget
+        self.start_page()
 
-        game_title = QtWidgets.QLabel()
+
+    def start_page(self):
+        start_frame = QtWidgets.QFrame(self)
+        start_frame.setGeometry(0, 0, self.width(), self.height())
+        self.start_frame = start_frame
+
+        game_title = QtWidgets.QLabel(start_frame)
         game_title.setAlignment(QtCore.Qt.AlignHCenter)
         game_title.setText("Hangman Game")
         title_font = QtGui.QFont()
@@ -33,25 +34,34 @@ class Application(QMainWindow):
         title_font.setBold(True)
         title_font.setUnderline(True)
         game_title.setFont(title_font)
-        start_layout.addWidget(game_title)
+        game_title.adjustSize()
+        game_title.move(int((self.width()-game_title.width())/2), 20)
+        self.game_title = game_title
 
-        background_image = QtWidgets.QLabel(self)
-        background_image.setGeometry(-70, 100, 900, 660)
+        background_image = QtWidgets.QLabel(start_frame)
         background_pixmap = QtGui.QPixmap("Images/start.png")
         background_image.setPixmap(background_pixmap)
+        background_image.adjustSize()
+        background_image.move(int((self.width()-background_image.width())/2), game_title.y()+game_title.height()+30)
+        self.background_image = background_image
 
-        start_button = QtWidgets.QPushButton(self)
+        start_button = QtWidgets.QPushButton(start_frame)
         start_button.setText("START GAME")
         start_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         button_font = QtGui.QFont(title_font)
         button_font.setPointSize(20)
         start_button.setFont(button_font)
         start_button.setStyleSheet("background-color: none")
-        w = 300
-        h = 80
-        start_button.setGeometry(int((self.width()-w)/2), int((self.height()-h)/2), w, h)
-        start_button.clicked.connect(self.get_word)
+        start_button.resize(300, 80)
+        start_button.move(int((self.width()-start_button.width())/2), int((self.height()-start_button.height())/2)+10)
+        def next_page():
+            self.start_frame.deleteLater()
+            self.get_word()
+
+        start_button.clicked.connect(next_page)
         self.start_button = start_button
+
+        start_frame.show()
 
 
     def get_word(self):
@@ -224,7 +234,10 @@ class Application(QMainWindow):
         next_button_font.setUnderline(False)
         next_button.setFont(next_button_font)
         next_button.setDisabled(True)
-        next_button.clicked.connect(lambda: self.word_guessing_page(textinput.text()))
+        def next_page():
+            self.get_word_frame.deleteLater()
+            self.word_guessing_page(textinput.text())
+        next_button.clicked.connect(next_page)
         self.next_button = next_button
 
         char_alert_2 = QtWidgets.QLabel(get_word_frame)
@@ -245,6 +258,9 @@ class Application(QMainWindow):
 
 
     def word_guessing_page(self, word):
+        self.word = word
+        self.guessing_time = time.time()
+
         guess_word_frame = QtWidgets.QFrame(self)
         guess_word_frame.setGeometry(0, 0, self.width(), self.height())
         self.guess_word_frame = guess_word_frame
@@ -296,11 +312,19 @@ class Application(QMainWindow):
         row = 0
 
         self.wrong_guess = 0
+        self.correct_letters = []
         def char_button_function(button):
             char = button.text()
             if char in word:
                 for label in all_labels[char]:
                     label.setText(char)
+                    self.correct_letters.append(char)
+                del all_labels[char]
+
+                if not all_labels:
+                    self.guessing_time = round(time.time() - self.guessing_time, 0)
+                    self.guess_word_frame.deleteLater()
+                    self.end_page(True)
 
             else:
                 if self.wrong_guess < 10:
@@ -314,7 +338,10 @@ class Application(QMainWindow):
                     pixmap = QtGui.QPixmap(f"Images/Hangman{self.wrong_guess+1}.png")
                     hangman_label.setPixmap(pixmap)
                     hangman_label.adjustSize()
-                    print("game over")
+
+                    self.guessing_time = round(time.time() - self.guessing_time, 0)
+                    self.guess_word_frame.deleteLater()
+                    self.end_page(False)
 
             button.setDisabled(True)
 
@@ -338,11 +365,7 @@ class Application(QMainWindow):
             hangman_label = QtWidgets.QLabel(guess_word_frame)
             hangman_label.setAlignment(QtCore.Qt.AlignBottom)
             self.hangman_spacing = 10
-            self.hangman_initial_size = (round(self.width()/2, 0), round(self.height()/2, 0))
             self.hangman_label = hangman_label
-
-
-
 
 
         guess_word_frame.show()
@@ -350,10 +373,76 @@ class Application(QMainWindow):
 
 
 
+    def end_page(self, win: bool, details=[]):
+        end_frame = QtWidgets.QFrame(self)
+        end_frame.setGeometry(0, 0, self.width(), self.height())
+        self.end_frame = end_frame
+
+        if win:
+            image = "Images/victory.png"
+        else:
+            image = "Images/defeat.png"
+
+        result_label = QtWidgets.QLabel(end_frame)
+        result_label.setPixmap(QtGui.QPixmap(image))
+        result_label.adjustSize()
+        result_label.move(int((self.width()-result_label.width())/2), int((self.height()-result_label.height())/2))
+        self.result_label = result_label
+
+        details_label = QtWidgets.QLabel(end_frame)
+        details_label.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        details_label.setAlignment(QtCore.Qt.AlignTop)
+        details_label_font = QtGui.QFont()
+        details_label_font.setFamily("Bahnschrift")
+        details_label_font.setBold(True)
+        details_label_font.setPointSize(17)
+        details_label.setFont(details_label_font)
+        details_label.setText(f"- Chosen word: {self.word}\n"
+                              f"- You guessed {round((len(self.correct_letters)/len(self.word))*100, 2)}% of given word\n"
+                              f"- You made {self.wrong_guess} / 10 mistakes\n"
+                              f"- You were playing for: {int(self.guessing_time)} seconds")
+
+        details_label.adjustSize()
+        details_label.move(10, int(self.height()/1.7))
+        self.details_label = details_label
+
+
+        new_game_button = QtWidgets.QPushButton(end_frame)
+        new_game_button.setStyleSheet("background-color: none")
+        new_game_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.button_margin = 50
+        new_game_button.resize(200, 50)
+        new_game_button.move(int((details_label.width()-new_game_button.width())/2), details_label.y()+details_label.height()+self.button_margin)
+        new_game_button_font = QtGui.QFont(details_label_font)
+        new_game_button_font.setPointSize(20)
+        new_game_button.setFont(new_game_button_font)
+        new_game_button.setText("PLAY AGAIN")
+        def new_game():
+            self.end_frame.deleteLater()
+            self.start_page()
+
+        new_game_button.clicked.connect(new_game)
+        self.new_game_button = new_game_button
+
+
+
+        end_frame.show()
+
+
+
+
+
+
 
     def resizeEvent(self, event):
-        self.start_layout_widget.resize(self.width(), self.height())
-        self.start_button.move(int((self.width()-self.start_button.width())/2)-1, int((self.height()-self.start_button.height())/2)+19)
+        try:
+            self.game_title.move(int((self.width()-self.game_title.width())/2), 20)
+            self.background_image.move(int((self.width() - self.background_image.width()) / 2), self.game_title.y() + self.game_title.height() + 30)
+            self.start_button.move(int((self.width() - self.start_button.width()) / 2), self.start_button.y())
+
+
+        except:
+            pass
 
         try:
             self.get_word_frame.resize(self.width(), self.height())
@@ -372,6 +461,14 @@ class Application(QMainWindow):
             self.word_widget.setGeometry(0, int(self.height()/4), self.width(), 100)
             self.letters_widget.setGeometry(int(self.width()/2), int(self.height()/2), int(self.width()/2), int(self.height()/2))
             self.hangman_label.move(self.hangman_spacing, self.height()-self.hangman_label.height()-self.hangman_spacing)
+        except:
+            pass
+
+        try:
+            self.end_frame.resize(self.width(), self.height())
+            self.result_label.move(int((self.width()-self.result_label.width())/2), int((self.height()-self.result_label.height())/2))
+            self.details_label.move(10, int(self.height()/1.7))
+            self.new_game_button.move(int((self.details_label.width()-self.new_game_button.width())/2), self.details_label.y()+self.details_label.height()+self.button_margin)
 
         except:
             pass
